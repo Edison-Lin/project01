65a6886a2ef1694fa5e1be2d60557c089468cdd2
esonboy <esonboy@gmail.com>
2025-03-19 01:54:08 +0800

    12343

--- Connections/conn_db.php
+++ Connections/conn_db.php
@@ -1,8 +1,8 @@
 <?php
 //PDO sql資料庫連線程式
-$dsn = "mysql:host=sql202.infinityfree.com/;dbname=if0_38544621_expstore;charset=utf8";
+$dsn = "mysql:host=sql202.infinityfree.com;dbname=if0_38544621_expstore;charset=utf8";
 $user="if0_38544621";
-$password="Wl02257385";
+$password="Ctf0Lix6fGkh";
 $link=new PDO($dsn,$user,$password);
 date_default_timezone_set("Asia/Taipei");
 //php 5.3.6 以前版本
